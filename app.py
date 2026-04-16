ed5efcbdff1b0c11882c8d569de0cc861421b9a4
Karthikt-2005 <162602604+Karthikt-2005@users.noreply.github.com>
2026-04-16 21:51:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
 
-# Load the trained model
-model = tf.keras.models.load_model('model.h5')
 
-# Load the encoders and scaler
+model = tf.keras.models.load_model('model.h5', compile=False)
+
+
 with open('label_encoder_gender.pkl', 'rb') as file:
     label_encoder_gender = pickle.load(file)
 
@@ -19,7 +19,7 @@ with open('scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
 
-## streamlit app
+
 st.title('Customer Churn PRediction')
 
 # User input
@@ -34,7 +34,7 @@ num_of_products = st.slider('Number of Products', 1, 4)
 has_cr_card = st.selectbox('Has Credit Card', [0, 1])
 is_active_member = st.selectbox('Is Active Member', [0, 1])
 
-# Prepare the input data
+
 input_data = pd.DataFrame({
     'CreditScore': [credit_score],
     'Gender': [label_encoder_gender.transform([gender])[0]],
@@ -47,18 +47,18 @@ input_data = pd.DataFrame({
     'EstimatedSalary': [estimated_salary]
 })
 
-# One-hot encode 'Geography'
+
 geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
 geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
 
-# Combine one-hot encoded columns with input data
+
 input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
 
-# Scale the input data
+
 input_data_scaled = scaler.transform(input_data)
 
 
-# Predict churn
+
 prediction = model.predict(input_data_scaled)
 prediction_proba = prediction[0][0]
 
